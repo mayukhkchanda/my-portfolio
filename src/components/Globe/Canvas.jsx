ee4f0bda9dd9644a07e2bc7e73eb786a00b4eedc
mayukhkchanda <mayukhkchanda@gmail.com>
2023-01-29 19:01:50 +0530

    fix social styles

--- src/components/Globe/Canvas.jsx
+++ src/components/Globe/Canvas.jsx
@@ -1,13 +1,11 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import Scene from "./Scene";
 import Model from "./GLTF";
 
 const canvas = () => {
   return (
     <Canvas>
       <Suspense fallback={"Loading..."}>
-        {/* <Scene /> */}
         <Model />
       </Suspense>
     </Canvas>
